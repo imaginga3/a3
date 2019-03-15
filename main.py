ed19bcd96bae3dce014af931e6f0f44f88c686db
Daniel Gordon <dgordo03@uoguelph.ca>
2019-03-15 18:13:35 -0400

    up to foreground determination is done

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
+import math
 import imageio
 import numpy as np
 # Moving object detection with background subtraction and shadow removal
+def foreground(I,lambda_k,mad):
+    w = np.array([[1,2,1],[2,4,2],[1,2,1]])
+    h = 3
+    # 1. window * abs(calc I - lambda_k)
+    # 2. h * window * lambda_k
+    print w
 def MAD(I,M):
     mad = np.subtract(I,M)
     mad = np.absolute(mad)
+    mad = np.median([mad[0],mad[1],mad[2],mad[3],mad[4]],axis=0)
     mad *= 1.4826
     return mad
 def alphaMetricallyTrimmedMean(I,f):
@@ -13,21 +21,12 @@ def alphaMetricallyTrimmedMean(I,f):
     f = np.multiply(I,f)
     f = np.sum([f[0],f[1],f[2],f[3],f[4]], axis=0)
     lambda_k = np.divide(f, lambda_k)
-    print lambda_k
-    # lambda_k = np.ceil(lambda_k)
-    # lambda_k = np.divide(I,lambda_k)
-    # return lambda_k
-    pass
+    return lambda_k
 def integerFunction(I,M):
     f = np.subtract(I,M)
     f = np.absolute(f)
     f = np.equal(f, 0)
     f = f.astype(int)
-    # f[1] = np.multiply(f[1], 2)
-    # f[2] = np.multiply(f[2], 3)
-    # f[3] = np.multiply(f[3], 4)
-    # f[4] = np.multiply(f[4], 5)
-    # f = np.amax([f[0],f[1],f[2],f[3],f[4]],axis=0)
     return f
 def temporalMedian(T):
     M = np.median([T[0],T[1],T[2],T[3],T[4]],axis=0)
@@ -42,12 +41,9 @@ def temporalIntensity(dir):
 def main():
     I = temporalIntensity('images/home/')
     M = temporalMedian(I)
-    # imageio.imwrite('median.jpg', M)
     f = integerFunction(I,M)
-    # imageio.imwrite('integerFunction.jpg', f)
     lambda_k = alphaMetricallyTrimmedMean(I,f)
-    # imageio.imwrite('lambda_k.jpg', lambda_k)
-    # mad = MAD(I,M)
-    # imageio.imwrite('mad.jpg', mad )
+    mad = MAD(I,M)
+    foreground(I,lambda_k,mad)
 if __name__ == "__main__":
     main()

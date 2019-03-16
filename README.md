8f4757cfbdcd26bc902dc56f6f4b4748ba7188ba
Brad Scott <rscott10@mail.uoguelph.ca>
2019-03-16 17:58:36 -0400

    Comments added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # a3
 A3-Removing People from holiday snaps
+
+#required
+
+Python 2.7
+scikit-image
+numpy
+PIL
+opencv2
+os
+math
+Tkinter
--- main.py
+++ main.py
@@ -54,12 +54,14 @@ def foreground(I,lambda_k,mad):
                 mask[x+1,y+1,1] = 0
                 mask[x+1,y+1,2] = 0
     return mask
+
 def MAD(I,M):
     mad = np.subtract(I,M)
     mad = np.absolute(mad)
     mad = np.median([mad[0],mad[1],mad[2],mad[3],mad[4]],axis=0)
     mad *= 1.4826
     return mad
+
 def alphaMetricallyTrimmedMean(I,f):
     lambda_k = np.sum([f[0],f[1],f[2],f[3],f[4]], axis=0)
     temp = np.multiply(lambda_k,0.3)
@@ -68,15 +70,18 @@ def alphaMetricallyTrimmedMean(I,f):
     temp = np.sum([temp[0],temp[1],temp[2],temp[3],temp[4]], axis=0)
     lambda_k = np.divide(temp, lambda_k)
     return lambda_k
+
 def integerFunction(I,M):
     f = np.subtract(I,M)
     f = np.absolute(f)
     f = np.equal(f, 0)
     f = f.astype(int)
     return f
+
 def temporalMedian(T):
     M = np.median([T[0],T[1],T[2],T[3],T[4]],axis=0)
     return M
+
 def temporalIntensity(dir):
     images = os.listdir(dir)
     I = [0 for x in xrange(len(images))]
@@ -84,23 +89,30 @@ def temporalIntensity(dir):
         img = imageio.imread(dir + images[image])
         I[image] = img
     return I
+
 def backgroundSubtraction():
-    I = temporalIntensity('images/home/')
-    M = temporalMedian(I)
-    f = integerFunction(I,M)
-    lambda_k = alphaMetricallyTrimmedMean(I,f)
-    imageio.imwrite("test.jpg", lambda_k)
     I = temporalIntensity('images/Photos/')
     M = temporalMedian(I)
     f = integerFunction(I,M)
     lambda_k = alphaMetricallyTrimmedMean(I,f)
+
+    imageio.imwrite("test.jpg", lambda_k)
+
+    I = temporalIntensity('images/Photos/')
+    M = temporalMedian(I)
+    f = integerFunction(I,M)
+    lambda_k = alphaMetricallyTrimmedMean(I,f)
+
     imageio.imwrite("test2.jpg", lambda_k)
     # mad = MAD(I,M)
     # mask = foreground(I,lambda_k,mad)
+
 def shadowSubtraction():
     pass
+
 def main():
     backgroundSubtraction()
     # shadowSubtraction()
+
 if __name__ == "__main__":
     main()

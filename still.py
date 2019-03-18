5cf4e2464fceb48ad22687d49813fe221abfdf8c
RobertBScott <rscott10@mail.uoguelph.ca>
2019-03-17 20:57:14 -0400

    still module

--- still.py
+++ still.py
@@ -5,9 +5,5 @@ import cv2 as cv
 
 def removeSelectedObject(image,TopLeft,TopRight,BottomLeft,BottomRight):
 
-    #removeSelectedObject
-    for x in range(TopLeft,TopRight):
-        for y in range(TopLeft,BottomLeft):
-            
 
     return

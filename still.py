117e4852b33f47f4a486f436671125620f2ec125
Brad Scott <rscott10@mail.uoguelph.ca>
2019-03-18 13:35:50 -0400

    Added paper. Begun pseudocode.

--- still.py
+++ still.py
@@ -12,6 +12,19 @@ def removeSelectedObject(image,x0,y0,x1,y1):
 
     cv.imwrite('Removed.png',image)
 
-    
+    # Patched-based inpainting
+    # Rajesh Pandurang Borole and Sanjiv Vedu BondePatch-Based Inpainting for Object Removal and Region Filling in Images
+    # DOI 10.1515/jisys-2013-0031  Journal of Intelligent Systems 2013; 22(3): 335–350
 
+    """
+    create list of target area pixels
+
+    iterate until no target pixels remain
+        assign pixel priorities
+            compute pixel confidence within patch
+            compute isophote priorty?
+            compute pixel priority
+        sort list by priority
+        inpaint highest priorty pixel
+    """
     return

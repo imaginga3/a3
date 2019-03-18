c5d970f8af70ec88c108a05cb821cd80451ec68e
RobertBScott <rscott10@mail.uoguelph.ca>
2019-03-17 20:56:42 -0400

    Added python module for still object removal

---
+++ still.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+import numpy as np
+import cv2 as cv
+
+def removeSelectedObject(image,TopLeft,TopRight,BottomLeft,BottomRight):
+
+    #removeSelectedObject
+    for x in range(TopLeft,TopRight):
+        for y in range(TopLeft,BottomLeft):
+            
+
+    return

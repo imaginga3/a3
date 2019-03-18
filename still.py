eac54a2302f115beae2ee582d493e1e9b1997afc
Brad Scott <rscott10@mail.uoguelph.ca>
2019-03-18 13:21:28 -0400

    Remove selected box area of image based on Top Left x,y and Bottom Right x,y

--- still.py
+++ still.py
@@ -3,7 +3,15 @@ import cv2
 import numpy as np
 import cv2 as cv
 
-def removeSelectedObject(image,TopLeft,TopRight,BottomLeft,BottomRight):
+def removeSelectedObject(image,x0,y0,x1,y1):
 
+    #remove selected region, assume square box
+    for x in range(x0,x1):
+        for y in range(y0,y1):
+            image[y,x] = 0
+
+    cv.imwrite('Removed.png',image)
+
+    
 
     return

3c681a2bddc79a86cd296985be9f1d10ef45a647
Daniel Gordon <dgordo03@uoguelph.ca>
2019-04-05 22:29:08 -0400

    updated moving objects, works better in most cases, more and harder image sets added

--- movingObjects.py
+++ movingObjects.py
@@ -48,95 +48,68 @@ def hysterisesThresholding(diff,t_h,t_l):
     t = thresh[:,:,2] == 1
     new_thresh[t] = 1
     return new_thresh
-def objectDetection(frames,bg):
-    # Scale the image to 1/8 of the size of original image
-    scale = 8
-    small_frames = [0 for x in xrange(len(frames))]
-    for i in xrange(len(small_frames)):
-        small_frames[i] = resizeImage(frames[i],scale)
-    small_B = resizeImage(bg,scale)
-    diff = [0 for x in xrange(len(frames))]
-    small_frames = np.array(small_frames,copy=True,dtype=np.int16)
-    small_B = np.array(small_B,copy=True,dtype=np.int16)
-    # Determine Diff(x,y) = |I(x,y) - B(x,y)|
-    for i in xrange(len(small_frames)):
-        diff[i] = cv2.absdiff(small_frames[i],small_B)
-        diff[i] = resizeImage(diff[i],8,cv2.INTER_CUBIC)
+def removeForeground(frames,background,time):
+    bg = np.array(background, copy=True, dtype=np.int16)
     mask = [0 for x in xrange(len(frames))]
-    kernel = np.ones((5,5),np.uint8)
-    # Perform hysterises thresholding
-    for i in xrange(len(diff)):
-        threshold = hysterisesThresholding(diff[i],10,5)
-        temp = np.array(frames[i],copy=True)
-        temp[:,:,0] = np.multiply(temp[:,:,0],threshold)
-        temp[:,:,1] = np.multiply(temp[:,:,1],threshold)
-        temp[:,:,2] = np.multiply(temp[:,:,2],threshold)
-        temp = cv2.erode(temp,kernel,iterations = 5)
-        temp = cv2.dilate(temp,kernel,iterations = 5)
-        mask[i] = temp
-    bg = np.array(bg,copy=True,dtype=np.int16)
-    # Separate the foregound and background from one another
-    fg = [0 for x in xrange(len(frames))]
-    for i in xrange(len(mask)):
-        fg[i] = np.array(mask[i],copy=True,dtype=np.int16)
-        bg_mask = np.greater(fg[i],0)
-        bg_mask = bg_mask.astype(int)
-        fg[i] = np.multiply(bg,bg_mask)
-    foreground = np.mean([fg[0],fg[1],fg[2],fg[3]],axis=0)
-    fgg = np.isclose(foreground,bg,atol=1)
-    fgg = fgg.astype(int)
-    foreground = np.multiply(foreground,fgg)
-    foreground = cv2.dilate(foreground,kernel,iterations = 3)
-    t = foreground > 0
-    foreground[t] = 255
-    r = np.equal(foreground[:,:,0],255)
-    g = np.equal(foreground[:,:,1],255)
-    b = np.equal(foreground[:,:,2],255)
-    nd = np.logical_and(r,g)
-    nd = nd.astype(int)
-    nd = np.logical_and(nd,b)
-    nd = nd.astype(int)
-    foreground[:,:,0] = np.multiply(foreground[:,:,0],nd)
-    foreground[:,:,1] = np.multiply(foreground[:,:,1],nd)
-    foreground[:,:,2] = np.multiply(foreground[:,:,2],nd)
-    foreground = np.multiply(foreground,255)
-    foreground = np.greater(foreground,0)
-    foreground = foreground.astype(int)
-    object = np.multiply(foreground,bg)
-    object = np.array(object,copy=True,dtype=np.int16)
-    return object
+    kernel = np.ones((3,3),np.uint8)
+    for i in xrange(len(frames)):
+        # Determine the foreground
+        fr = np.array(frames[i],copy=True,dtype=np.int16)
+        t = np.isclose(bg,fr,atol=15)
+        t = t.astype(int)
+        t = np.all(t != [0,0,0], axis=-1)
+        # Using neighbours of 8, remove all those that shouldn't be foreground
+        ########################################################
+        t = np.array(t, copy=True, dtype=np.uint8)
+        # kernel = np.array([[1,1,1],[1,0,1],[1,1,1]])
+        # thresh = cv2.filter2D(t,-1,kernel)
+        # thresh = np.greater_equal(thresh,1)
+        # thresh = thresh.astype(int)
+        # mask[i] = t
+        ########################################################
+        # mask[i] = cv2.erode(t,kernel,iterations = 1)
+        # temp = cv2.dilate(temp,kernel,iterations = 5)
+        # mask[i] = cv2.morphologyEx(t, cv2.MORPH_CLOSE, kernel)
+        mask[i] = cv2.morphologyEx(t, cv2.MORPH_OPEN, kernel)
+    # Get the temporal median of all backgrounds computed
+    bg_mask = temporalMedianBlending(mask)
+    bg_mask = np.multiply(bg_mask, 255)
+    # Further separate the foregound from the background, keeping only those pixels
+    # that are black across all iterations
+    bg_mask = np.greater(bg_mask, 0)
+    bg_mask = bg_mask.astype(int)
+    # Grab the background from the original image
+    new_bg = np.zeros((background.shape[0],background.shape[1],background.shape[2]))
+    new_bg[:,:,0] = np.multiply(bg[:,:,0], bg_mask)
+    new_bg[:,:,1] = np.multiply(bg[:,:,1], bg_mask)
+    new_bg[:,:,2] = np.multiply(bg[:,:,2], bg_mask)
+    return new_bg
 def generateBackground(objects,frames):
-    bg = [0 for x in xrange(len(frames))]
-    # Only grab the background for each of the frames
-    for i in xrange(len(bg)):
-        fg_mask = np.equal(objects[i],0)
-        fg_mask = fg_mask.astype(int)
-        bg[i] = np.multiply(frames[i],fg_mask)
     # Grab the temporal median of all frames of background
-    background = temporalMedianBlending(bg)
+    new_bg = temporalMedianBlending(objects)
     # Sometimes there will be nothing remaining in some pixels, find these pixels,
-    # and using inpainting to fill
-    mask = np.equal(background,0)
+    # use the image which resembles the current background most
+    median = temporalMedianBlending(frames)
+    mask = np.equal(new_bg,0)
     mask = mask.astype(int)
-    mask = np.multiply(mask,255)
-    mask = mask[:,:,0]
-    mask = np.array(mask,copy=True,dtype=np.uint8)
-    background = np.array(background,copy=True,dtype=np.uint8)
-    background = cv2.inpaint(background,mask,3,cv2.INPAINT_TELEA)
-    return background
+    median = np.multiply(median, mask)
+    # Add the good background and the remainder together
+    new_bg = np.add(new_bg, median)
+    # new_bg = np.add(new_bg, closest)
+    return new_bg
 def reconstruct(dir,output):
     # Get all frame intensities to be used
     frames = frameIntensities(dir)
-    # Find the initial 'bad' background
     median = temporalMedianBlending(frames)
-    objects = [0 for x in xrange(len(frames))]
+    backgrounds = [0 for x in xrange(len(frames))]
     # Iteratively remove the current frame to compare against the remainder of the frames
     # in order to threshold out the foreground from background
     for i in xrange(len(frames)):
         bg = frames[i]
         fr = np.delete(frames,i,0)
         # Separate the foregound and background
-        objects[i] = objectDetection(fr,bg)
+        backgrounds[i] = removeForeground(fr,bg,i)
+        cv2.imwrite(str(i)+".jpg",backgrounds[i])
     # Using the generated backrounds, get the overall background
-    out = generateBackground(objects,frames)
+    out = generateBackground(backgrounds,frames)
     cv2.imwrite(output,out)
